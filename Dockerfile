2479ff87f0e3bc29d2c817111c5e83e020a1a1c4
Schwannden-Kuo <Schwannden.Kuo@dell.com>
2023-08-30 21:56:15 +0800

    feat(api): add login mock api

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,11 @@ FROM python:3.11-bullseye
 
 WORKDIR /app
 
-COPY ./pyproject.toml /app/pyproject.toml
+RUN pip install --upgrade pip
+RUN pip install poetry
+RUN poetry config virtualenvs.create false
 
-RUN pip install --no-cache-dir --upgrade pip
-RUN pip install --no-cache-dir poetry
-RUN poetry env use system
+COPY ./pyproject.toml /app/pyproject.toml
 RUN poetry install
 
-COPY ./k3d_demo_be /app
-
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "5000"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "5000", "--reload"]
--- k3d_demo_be/main.py
+++ k3d_demo_be/main.py
@@ -1,8 +1,37 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.logger import logger
+from pydantic import BaseModel
+
+class LoginBody(BaseModel):
+    username: str
+    password: str
+
+class User(BaseModel):
+    name: str
+    userid: str
+    email: str
+    access: str
+    avatar: Optional[str] = None
+
+class LoginResponse(BaseModel):
+    data: User
+    status: str
+
+current_user: User = User(
+    name="demo",
+    userid="demo_id",
+    email="demo@dell.com",
+    access="admin",
+)
 
 app = FastAPI()
 
+@app.get("/api/currentUser")
+async def get_current_user() -> LoginResponse:
+    return LoginResponse(data=current_user, status="ok")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/api/login/account")
+async def login_account(login_body: LoginBody) -> LoginResponse:
+    logger.info(f"received login from {login_body.username}")
+    return LoginResponse(data=current_user, status="ok")
